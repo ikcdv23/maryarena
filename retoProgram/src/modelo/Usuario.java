52d493c4f65c33565bec00c5c2b90f69fe6e2208
Melissa Vesga <ikdgv@plaiaundi.net>
2025-01-27 09:46:36 +0100

    commit de javi, no hacer merge

--- retoProgram/src/modelo/Usuario.java
+++ retoProgram/src/modelo/Usuario.java
@@ -2,63 +2,65 @@ package modelo;
 
 public class Usuario {
 
-	private String dni;
-	private String nombre;
-	private String apellido;
-	private String contraseña;
-	private rolUsuario rol;
-	
-	
-	public Usuario() {
-		super();
-	}
-	
-	public Usuario(String dni, String nombre, String apellido, String contraseña, rolUsuario cliente) {
-		super();
-		this.dni = dni;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.contraseña = contraseña;
-<<<<<<< HEAD
-		this.rol = cliente;
-=======
-		this.rol = rol;
->>>>>>> branch 'master' of https://github.com/ikcdv23/maryarena.git
-	}
-	public String getDni() {
-		return dni;
-	}
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getContraseña() {
-		return contraseña;
-	}
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
-	}
-	public rolUsuario getCliente() {
-		return rol;
-	}
-	public void setCliente(rolUsuario cliente) {
-<<<<<<< HEAD
-		this.rol = cliente;
-=======
-		this.rol = rol;
->>>>>>> branch 'master' of https://github.com/ikcdv23/maryarena.git
-	}
+    // Atributos privados
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String contraseña;
+    private rolUsuario rol;
 
-	
+    // Constructores
+    public Usuario() {
+        super();
+    }
+
+    public Usuario(String dni, String nombre, String apellido, String contraseña, rolUsuario rol) {
+        super();
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+    // Getters y Setters
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public rolUsuario getRol() {
+        return rol;
+    }
+
+    public void setRol(rolUsuario rol) {
+        this.rol = rol;
+    }
 }
--- retoProgram/src/repositorios/ConectorBD.java
+++ retoProgram/src/repositorios/ConectorBD.java
@@ -1,6 +1,7 @@
 package repositorios;
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 
 import modelo.Usuario;
