ff23b47243ebacbcc79bbec112e3f60ca48ef282
Anna Batuieva <ikdgf@plaiaundi.net>
2025-01-27 11:58:36 +0100

    herencias neoprenos, tablasurf

---
+++ retoProgram/src/modelo/Neopreno.java
@@ -0,0 +1,39 @@
+package modelo;
+
+public class Neopreno extends Articulo{
+    private String grosor;
+    private String color;   
+    private String talla;
+	public Neopreno(int idArticulo, double precio_horas, boolean disponibilidad, int idOficina, String grosor,
+			String color, String talla) {
+		super(idArticulo, precio_horas, disponibilidad, idOficina);
+		this.grosor = grosor;
+		this.color = color;
+		this.talla = talla;
+	}
+	public String getGrosor() {
+		return grosor;
+	}
+	public void setGrosor(String grosor) {
+		this.grosor = grosor;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getTalla() {
+		return talla;
+	}
+	public void setTalla(String talla) {
+		this.talla = talla;
+	}
+	@Override
+	public String toString() {
+		return "Neopreno [grosor=" + grosor + ", color=" + color + ", talla=" + talla + ", "
+				+ super.toString() + "]";
+	}   
+    
+    
+}
