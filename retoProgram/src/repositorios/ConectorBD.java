bdc46eee551503401f26b6862e67338273c0b9fb
Anna Batuieva <ikdgf@plaiaundi.net>
2025-01-27 08:48:53 +0100

    conector añadido en su clase y en el main.

--- retoProgram/src/repositorios/ConectorBD.java
+++ retoProgram/src/repositorios/ConectorBD.java
@@ -1,5 +1,32 @@
 package repositorios;
 
-public class ConectorBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+import modelo.Usuario;
+
+public class ConectorBD {
+	
+private static Connection conexion;
+
+	//Usuario usuario = new Usuario();
+    
+    public static void conectar(){
+
+        try{
+        	
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver cargado");        
+            try{
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/MaryArena","root","1DAW3_BBDD");
+          
+            System.out.println("Conexion establecida");
+          
+        }catch(Exception e){
+            System.out.println("Error en la conexion");
+        }
+        }catch(Exception e){
+            System.out.println("Error en el driver");
+        }
+    }
 }
