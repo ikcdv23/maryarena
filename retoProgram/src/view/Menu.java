8d8d01bb709978eb675c3b5247b641f0ee899037
Melissa Vesga <ikdgv@plaiaundi.net>
2025-01-29 10:15:41 +0100

    usuarios obtiene rol

--- retoProgram/src/view/Menu.java
+++ retoProgram/src/view/Menu.java
@@ -1,5 +1,6 @@
 package view;
 
+import modelo.Usuario;
 import repositorios.RepositorioUsuario;
 
 public class Menu {
@@ -7,31 +8,50 @@ public class Menu {
 	public void mostrarMenu() {
 
 		int opcion;
+		int menuClienteUsuario = 0;
 		do {
 			// Mostrar menu principal
 			System.out.println("Bienvenido al sistema de reservas");
 			System.out.println("1. Registrarse");
 			System.out.println("2. Iniciar sesión");
-			System.out.println("3. Salir");
+			System.out.println("4. Salir");
 
 			opcion = RepositorioUsuario.guardarOpcion();
 
 			switch (opcion) {
 			case 1:
 				System.out.println("Registrarse:");
-				RepositorioUsuario.registrarUsuario(); // Llamamor al registro
+				 // Llamamos al registro
+				Usuario usuario = RepositorioUsuario.registrarUsuario();
 				break;
 			case 2:
 				System.out.println("Iniciar sesión:");
 				RepositorioUsuario.iniciarSesion(); // Llamar al inicio de sesion
+				
+				menuClienteUsuario = 1;
 				break;
 			case 3:
+				System.out.println("Programa finalizado");
+				break;
+			case 4:
 				System.out.println("Programa finalizado");
 				break;
 			default:
 				System.out.println("Opción no válida. Intente de nuevo.");
 				break;
 			}
-		} while (opcion != 3); // El ciclo se repite hasta que el usuario elige "Salir"
+		} while (opcion != 4 || menuClienteUsuario == 1); // El ciclo se repite hasta que el usuario elige "Salir"
 	}
+
+	public static void menuReservas() {
+		// Mostrar menu principal
+		System.out.println("Bienvenido al sistema de reservas");
+		System.out.println("1. Mostrar mi perfil");
+		System.out.println("2. Crear reserva");
+		System.out.println("1. Comprobar reserva");
+		System.out.println("2. Ver productos");
+		System.out.println("3. Eliminar cuenta");
+		System.out.println("4. Cerrar sesión");
+	}
+
 }
