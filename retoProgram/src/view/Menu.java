1e4a5b129b23e09a47471b96ca1d61a40e034cd3
Melissa Vesga <ikdgv@plaiaundi.net>
2025-01-29 08:47:38 +0100

    iniciar sesion y registrar eliminado cambios en menu

--- retoProgram/src/view/Menu.java
+++ retoProgram/src/view/Menu.java
@@ -1,46 +1,37 @@
 package view;
-import repositorios.RepositorioUsuario;
 
-import registros.iniciarSesion;
-import registros.registrar;
 import repositorios.RepositorioUsuario;
-import utilidades.GestionDatos;
 
 public class Menu {
 
-    public void mostrarMenu() {
-    
-    	 int opcion;
-         do {
-             // Mostrar menu principal
-             System.out.println("Bienvenido al sistema de reservas");
-             System.out.println("1. Registrarse");
-             System.out.println("2. Iniciar sesión");
-             System.out.println("3. Salir");
+	public void mostrarMenu() {
 
-             opcion = GestionDatos.guardarOpcion(); // Guardar opción del usuario
+		int opcion;
+		do {
+			// Mostrar menu principal
+			System.out.println("Bienvenido al sistema de reservas");
+			System.out.println("1. Registrarse");
+			System.out.println("2. Iniciar sesión");
+			System.out.println("3. Salir");
 
-             switch (opcion) {
-                 case 1:
-                     System.out.println("Registrarse:");
-                     RepositorioUsuario.registrarUsuario(); // Llamamor al registro
-                     break;
-                 case 2:
-                     System.out.println("Iniciar sesión:");
-                     RepositorioUsuario.iniciarSesion(); // Llamar al inicio de sesion
-                     break;
-                 case 3:
-                     System.out.println("Programa finalizado"); 
-                     break;
-                 default:
-                     System.out.println("Opción no válida. Intente de nuevo.");
-                     break;
-             }
-         } while (opcion != 3); // El ciclo se repite hasta que el usuario elige "Salir"
-     }
+			opcion = RepositorioUsuario.guardarOpcion();
+
+			switch (opcion) {
+			case 1:
+				System.out.println("Registrarse:");
+				RepositorioUsuario.registrarUsuario(); // Llamamor al registro
+				break;
+			case 2:
+				System.out.println("Iniciar sesión:");
+				RepositorioUsuario.iniciarSesion(); // Llamar al inicio de sesion
+				break;
+			case 3:
+				System.out.println("Programa finalizado");
+				break;
+			default:
+				System.out.println("Opción no válida. Intente de nuevo.");
+				break;
+			}
+		} while (opcion != 3); // El ciclo se repite hasta que el usuario elige "Salir"
+	}
 }
-
-
-
-
-
