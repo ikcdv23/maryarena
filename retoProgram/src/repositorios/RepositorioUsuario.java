21e488715e133c530de8532a29bb994b8d2a6560
Anna Batuieva <ikdgf@plaiaundi.net>
2025-01-29 10:47:08 +0100

    RepositorioUsuario ordenado

--- retoProgram/src/repositorios/RepositorioUsuario.java
+++ retoProgram/src/repositorios/RepositorioUsuario.java
@@ -9,6 +9,49 @@ import view.Menu;
 
 public class RepositorioUsuario {
 
+	
+	//Método para registrar usuario
+	public static void registrarUsuario() {
+		Usuario usuario = pedirDatosUsuario();
+		insertarUsuario(usuario);
+		}
+	
+	
+	//Método para pedir datos al al usuario	
+	private static Usuario pedirDatosUsuario() {
+		Scanner sc = new Scanner(System.in);
+		String dni;
+		do {
+			System.out.println("Introduce el DNI de Usuario (9 caracteres):");
+			dni = sc.nextLine();
+			if (dni.length() != 9) {
+				System.out.println("El DNI debe tener exactamente 9 caracteres.");
+			}
+		} while (dni.length() != 9);
+
+		System.out.println("Introduce el nombre de Usuario:");
+		String nombre = sc.nextLine();
+
+		System.out.println("Introduce el apellido de Usuario:");
+		String apellido = sc.nextLine();
+
+		System.out.println("Introduce la contraseña de Usuario:");
+		String contraseña = sc.nextLine();
+
+		String rol;
+		do {
+			System.out.println("Introduce el rol del Usuario (Administrador / Cliente):");
+			rol = sc.nextLine().toLowerCase();
+			if (!rol.equals("administrador") && !rol.equals("cliente")) {
+				System.out.println("Rol no válido. Debe ser 'Administrador' o 'Cliente'.");
+			}
+		} while (!rol.equals("administrador") && !rol.equals("cliente"));
+
+		return new Usuario(dni, nombre, apellido, contraseña, rol);
+	}
+	
+	
+	//Método para insertar el usuario en la base de datos.
 	public static void insertarUsuario(Usuario usuario) {
 		String query = "INSERT INTO Usuario (dni, nombre, apellido, contraseña, rol) VALUES (?, ?, ?, ?, ?)";
 		String queryCheck = "SELECT COUNT(*) FROM Usuario WHERE DNI = ?";
@@ -41,6 +84,8 @@ public class RepositorioUsuario {
         }
     }
 
+	
+	//Método para iniciar sesion
 	public static void iniciarSesion() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingrese su DNI:");
@@ -49,10 +94,10 @@ public class RepositorioUsuario {
 		String contraseña = sc.nextLine();
 
 		try {
-			boolean autenticado = comprobarUsuario(dni, contraseña);
+			boolean autenticado = comprobarUsuario(dni, contraseña); //Llama al método para comprobar si el DNI y contraseña coinciden en la base de datos.
 			if (autenticado) {
 				System.out.println("Sesión iniciada.");
-				Menu.mostrarMenu2();
+				Menu.mostrarMenu2();						//Si el usuario existe, llama al segundo menú.
 			} else {
 				System.out.println("DNI o contraseña incorrectos. Inténtalo de nuevo.");
 			}
@@ -65,48 +110,8 @@ public class RepositorioUsuario {
 		}
 	}
 
-		public static void registrarUsuario() {
-			Usuario usuario = pedirDatosUsuario();
-			insertarUsuario(usuario);
-	}
-
-	private static Usuario pedirDatosUsuario() {
-		Scanner sc = new Scanner(System.in);
-		String dni;
-		do {
-			System.out.println("Introduce el DNI de Usuario (9 caracteres):");
-			dni = sc.nextLine();
-			if (dni.length() != 9) {
-				System.out.println("El DNI debe tener exactamente 9 caracteres.");
-			}
-		} while (dni.length() != 9);
-
-		System.out.println("Introduce el nombre de Usuario:");
-		String nombre = sc.nextLine();
-
-		System.out.println("Introduce el apellido de Usuario:");
-		String apellido = sc.nextLine();
-
-		System.out.println("Introduce la contraseña de Usuario:");
-		String contraseña = sc.nextLine();
-
-		String rol;
-		do {
-			System.out.println("Introduce el rol del Usuario (Administrador / Cliente):");
-			rol = sc.nextLine().toLowerCase();
-			if (!rol.equals("administrador") && !rol.equals("cliente")) {
-				System.out.println("Rol no válido. Debe ser 'Administrador' o 'Cliente'.");
-			}
-		} while (!rol.equals("administrador") && !rol.equals("cliente"));
-
-		return new Usuario(dni, nombre, apellido, contraseña, rol);
-	}
-
-	public static int guardarOpcion() {
-		Scanner sc = new Scanner(System.in);
-		return sc.nextInt();
-	}
-
+	
+	//Método para comprobar Usuario por DNI y contraseña
 	public static boolean comprobarUsuario(String dni, String contraseña) throws SQLException {
 		String queryCheck = "SELECT COUNT(*) FROM Usuario WHERE dni = ? AND contraseña = ?";
 		try (PreparedStatement checkStmt = ConectorBD.conexion.prepareStatement(queryCheck)) {
@@ -119,5 +124,12 @@ public class RepositorioUsuario {
 			e.printStackTrace();
 			throw e;
 		}
+	}
+
+	
+	//Método para guardar opción.
+	public static int guardarOpcion() {
+		Scanner sc = new Scanner(System.in);
+		return sc.nextInt();
 	}
 }
